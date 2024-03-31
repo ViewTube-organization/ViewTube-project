e9fb14faab1c42ffdaac6f723e1590389407f2ea
onweg <Innokentiydiscord@yandex.ru>
2024-03-31 19:12:49 +0300

    create project

---
+++ ViewTubeApp/ViewTubeApp/ViewTubeAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ViewTubeAppApp.swift
+//  ViewTubeApp
+//
+//  Created by Пользователь on 31.03.2024.
+//
+
+import SwiftUI
+
+@main
+struct ViewTubeAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
